f5f4ebb335701b30e72d1c4498eb278f87b4ffa8
Trevor Stone <github@trevorstone.org>
2016-11-29 23:19:18 -0700

    Let users enable and disable handle rules

--- src/app/handle-checker/handle-checker.component.ts
+++ src/app/handle-checker/handle-checker.component.ts
@@ -5,6 +5,15 @@ import { HandleConflict } from './handle-conflict';
 import { ALL_HANDLE_RULES, HandleRule } from './handle-rule';
 import { HandleService } from './handle.service';
 
+const RULE_NAMES = {
+  'american-soundex': 'American Soundex',
+  'edit-distance': 'Edit distance',
+  'fcc': 'FCC words',
+  'min-length': 'Minimum length',
+  'phonetic-alphabet': 'NATO phonetic letters',
+  'substring': 'Substring match',
+};
+
 @Component({
   selector: 'app-handle-checker',
   templateUrl: './handle-checker.component.html',
@@ -14,15 +23,19 @@ export class HandleCheckerComponent implements OnInit {
   handles: Handle[] = [];
   checkedNames: {name: string, conflicts: HandleConflict[]}[] = [];
   @Input() currentName = '';
+  @Input() ruleState: { rule: HandleRule, name: string, active: boolean }[] = [];
 
   constructor(
-    private handleService: HandleService,
-    @Inject(ALL_HANDLE_RULES) private allRules: HandleRule[]) {
+      private handleService: HandleService,
+      @Inject(ALL_HANDLE_RULES) private allRules: HandleRule[]) {
   }
 
   ngOnInit() {
     // TODO observable
     this.handleService.getHandles().then(handles => this.handles = handles);
+    this.ruleState = this.allRules.map((rule) => {
+      return {rule: rule, name: RULE_NAMES[rule.id] || rule.id, active: true};
+    }).sort((a, b) => a.name < b.name ? -1 : 1);
   }
 
   checkCurrentName() {
@@ -31,10 +44,12 @@ export class HandleCheckerComponent implements OnInit {
       return;
     }
     let conflicts = [];
-    for (let rule of this.allRules) {
-      rule.check(name).then((results) => {
-        Array.prototype.push.apply(conflicts, results);
-      });
+    for (let state of this.ruleState) {
+      if (state.active) {
+        state.rule.check(name).then((results) => {
+          Array.prototype.push.apply(conflicts, results);
+        });
+      }
     }
     this.checkedNames.unshift({name: name, conflicts: conflicts});
     this.currentName = '';
