3048f90cecf50f8986443d5c92726c7ef9bf5869
Trevor Stone <github@trevorstone.org>
2016-12-04 18:35:40 -0700

    Simplify component test code

--- src/app/handle-checker/handle-checker.component.spec.ts
+++ src/app/handle-checker/handle-checker.component.spec.ts
@@ -1,6 +1,7 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { dispatchEvent } from '@angular/platform-browser/testing/browser_util';
 import { DebugElement } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -10,6 +11,17 @@ import { ALL_HANDLE_RULES } from './handle-rule';
 import { MinLengthRule, FccRule, SubstringRule } from './handle-rules';
 import { HandleService } from './handle.service';
 import { MockHandleService } from './handle.service.mock';
+
+// TODO consider extracting these utility methods somewhere common
+function findElement(ancestor: DebugElement|ComponentFixture<any>, selector: string): DebugElement {
+  let debugElement = ancestor instanceof DebugElement ? ancestor : ancestor.debugElement;
+  return debugElement.query(By.css(selector));
+}
+
+function findElements(ancestor: DebugElement|ComponentFixture<any>, selector: string): DebugElement[] {
+  let debugElement = ancestor instanceof DebugElement ? ancestor : ancestor.debugElement;
+  return debugElement.queryAll(By.css(selector));
+}
 
 /** Jasmine unit tests for {@link HandleCheckerComponent}. */
 describe('HandleCheckerComponent', () => {
@@ -40,11 +52,11 @@ describe('HandleCheckerComponent', () => {
 
   it('should have a table of all handles', async(() => {
     fixture.detectChanges();
-    let table = fixture.debugElement.query(By.css('.all-handles'));
+    let table = findElement(fixture, '.all-handles');
     expect(table).toBeTruthy();
     fixture.whenStable().then(() => {
       fixture.detectChanges();
-      let names = table.queryAll(By.css('.handle-row .handle-name'));
+      let names = findElements(table, '.handle-row .handle-name');
       expect(names.length).toBe(26);
       expect(names[0].nativeElement.textContent.trim()).toEqual('Alfa');
       expect(names[25].nativeElement.textContent.trim()).toEqual('Zulu');
@@ -53,40 +65,40 @@ describe('HandleCheckerComponent', () => {
 
   it('should have a table of checked handles', async(() => {
     fixture.detectChanges();
-    let table = fixture.debugElement.query(By.css('.checked-handles'));
+    let table = findElement(fixture, '.checked-handles');
     expect(table).toBeTruthy();
     fixture.whenStable().then(() => {
       fixture.detectChanges();
-      let names = table.queryAll(By.css('.handle-row .handle-name'));
+      let names = findElements(table, '.handle-row .handle-name');
       expect(names.length).toBe(0); // starts empty
     });
   }));
 
   it('should check input handles', async(() => {
     fixture.detectChanges();
-    let input = fixture.debugElement.query(By.css('#handle-check-input'));
+    let input = findElement(fixture, '#handle-check-input');
     expect(input).toBeTruthy();
     let inputEl = input.nativeElement;
     expect(inputEl.textContent).toBe('');
-    let button = fixture.debugElement.query(By.css('button[type="submit"]'));
+    let button = findElement(fixture, 'button[type="submit"]');
     expect(button).toBeTruthy();
-    let table = fixture.debugElement.query(By.css('.checked-handles'));
+    let table = findElement(fixture, '.checked-handles');
     expect(table).toBeTruthy();
     fixture.whenStable().then(() => {
       fixture.detectChanges();
       inputEl.value = 'nov';
-      inputEl.dispatchEvent(new Event('input'));
+      dispatchEvent(inputEl, 'input');
       fixture.detectChanges();
       expect(component.currentName).toBe('nov');
       button.nativeElement.click();
       fixture.whenStable().then(() => {
         fixture.detectChanges();
-        let rows = table.queryAll(By.css('.handle-row'));
+        let rows = findElements(table, '.handle-row');
         expect(rows.length).toBe(1);
         let row = rows[0];
-        let nameCell = row.query(By.css('.handle-name'));
+        let nameCell = findElement(row, '.handle-name');
         expect(nameCell.nativeElement.textContent).toBe('nov');
-        let substringConflict = row.query(By.css('.handle-conflict-rule-substring'));
+        let substringConflict = findElement(row, '.handle-conflict-rule-substring');
         expect(substringConflict.nativeElement.textContent).toMatch('November');
         expect(inputEl.textContent).toBe(''); // submit clears input
       });
